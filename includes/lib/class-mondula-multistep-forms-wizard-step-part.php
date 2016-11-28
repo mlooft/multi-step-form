b8a473295eadd460d0915ba535e792bf9c441f74
Alexander Njemz <alexander@mondula.com>
2016-11-28 13:11:16 +0100

    Add paragraph

--- includes/lib/class-mondula-multistep-forms-wizard-step-part.php
+++ includes/lib/class-mondula-multistep-forms-wizard-step-part.php
@@ -92,6 +92,9 @@ class Mondula_Form_Wizard_Wizard_Step_Part {
                     case 'date':
                         $blocks[] = Mondula_Form_Wizard_Block_Date::from_aa( $block );
                         break;
+                    case 'paragraph':
+                        $blocks[] = Mondula_Form_Wizard_Block_Paragraph::from_aa( $block );
+                        break;
                     case 'submit':
                         $blocks[] = Mondula_Form_Wizard_Step_Submit::from_aa( $block );
                         break;
