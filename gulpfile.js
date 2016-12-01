a324575bb7ee3e2e09931ddf163277746e537865
Alexander Njemz <alexander@mondula.com>
2016-12-01 19:09:49 +0100

    Add zip task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var del = require('del');
 var concat = require('gulp-concat');
 var newer = require('gulp-newer');
 var uglify = require('gulp-uglify');
@@ -10,6 +11,7 @@ var livereload = require('gulp-livereload');
 var mainBowerFiles = require('main-bower-files');
 var wpPot = require('gulp-wp-pot');
 var sort = require('gulp-sort');
+var zip = require('gulp-zip');
 
 var base = 'assets/js';
 
@@ -116,3 +118,92 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['js', 'css', 'watch']);
+
+gulp.task('clean:production', function () {
+  return del('dist/**/*');
+});
+
+gulp.task('css-frontend:production', ['clean:production'], function () {
+  gulp.src(['assets/css/frontend/*.css', 'assets/css/frontend/*.less'])
+    .pipe(less())
+    .pipe(concat('frontend.min.css'))
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css-frontend-vendor:production', ['clean:production'], function () {
+  gulp.src(mainBowerFiles('**/*.css'))
+    .pipe(concat('vendor-frontend.min.css'))
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css-backend:production', ['clean:production'], function () {
+  gulp.src(['assets/css/backend/*.css', 'assets/css/backend/*.less'])
+    .pipe(less())
+    .pipe(concat('backend.min.css'))
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js-frontend:production', ['clean:production'], function () {
+  gulp.src(['assets/js/frontend/*.js'])
+    .pipe(concat('frontend.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js-frontend-vendor:production', ['clean:production'], function () {
+  gulp.src(mainBowerFiles('**/*.js'))
+    .pipe(concat('vendor-frontend.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js-backend:production', ['clean:production'], function () {
+  gulp.src(['assets/js/backend/*.js'])
+    .pipe(concat('backend.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles:production', ['css-frontend:production', 'css-frontend-vendor:production', 'css-backend:production']);
+
+gulp.task('scripts:production', ['js-frontend:production', 'js-frontend-vendor:production', 'js-backend:production']);
+
+gulp.task('build:production', ['scripts:production', 'styles:production']);
+
+gulp.task('clean:zip', function () {
+  return del('pkg/**/*');
+})
+
+gulp.task('copy:zip', ['clean:zip', 'build:production'], function () {
+  return gulp.src(
+      [
+        'dist/*', 
+        'includes/*', 
+        'lang/*', 
+        'LICENSE', 
+        'index.php',
+        'mondula-form-wizard.php',
+        'readme.md',
+        'readme.txt',
+        'uninstall.php' 
+      ], {base: '.'})
+    .pipe(gulp.dest('pkg/multi-step-form'));
+}); 
+
+gulp.task('zip', ['copy:zip'], function () {
+  return gulp.src('pkg/**/multi-step-form/**')
+    .pipe(zip('multi-step-form.zip'))
+    .pipe(gulp.dest('pkg'));
+});
