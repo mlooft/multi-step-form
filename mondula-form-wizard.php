1ca7988f2364a27cc3f5126a61c82e551e3f2581
Alexander Njemz <alexander@mondula.com>
2017-01-27 15:35:49 +0100

    Increment version

--- mondula-form-wizard.php
+++ mondula-form-wizard.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Multi Step Form
- * Version: 1.0.7
+ * Version: 1.0.8
  * Plugin URI: http://www.mondula.com/
  * Description: Create and embed Multi Step Form.
  * Author: Mondula GmbH
