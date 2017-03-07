8c6b286d93ef89a1e834c55d024dc95da814a968
Lewe Ohlsen <lohlsen@gmx.de>
2017-03-07 11:42:55 +0100

    versioning

--- mondula-form-wizard.php
+++ mondula-form-wizard.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Multi Step Form
- * Version: 1.0.8
+ * Version: 1.1.0
  * Plugin URI: http://www.mondula.com/
  * Description: Create and embed Multi Step Form.
  * Author: Mondula GmbH
