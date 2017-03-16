21ff0b0bebeae12717830a478c3bed44cc1f002f
leweohlsen <lohlsen@gmx.de>
2017-03-16 15:01:58 +0100

    version and changelog

--- mondula-form-wizard.php
+++ mondula-form-wizard.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Multi Step Form
- * Version: 1.1.4
+ * Version: 1.1.5
  * Plugin URI: http://www.mondula.com/
  * Description: Create and embed Multi Step Form.
  * Author: Mondula GmbH
@@ -9,7 +9,7 @@
  * Requires at least: 3.9
  * Tested up to: 4.7
  *
- * Text Domain: mondula-multistep-forms
+ * Text Domain: multi-step-form
  * Domain Path: /lang/
  *
  * @package WordPress
