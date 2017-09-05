3886c5350a79e631519c050ebad0dfbf4c0690d2
Lewe Ohlsen <mail@lewe.io>
2017-09-05 17:00:24 +0200

    indentation

--- includes/lib/class-mondula-multistep-forms-wizard-service.php
+++ includes/lib/class-mondula-multistep-forms-wizard-service.php
@@ -1,6 +1,8 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 
 /**
@@ -10,55 +12,59 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 class Mondula_Form_Wizard_Wizard_Service {
 
-    private $_repository;
+	private $_repository;
 
-    private $_plugin_version;
+	private $_plugin_version;
 
-    public function __construct( Mondula_Form_Wizard_Wizard_Repository $repository, $plugin_version ) {
-        $this->_repository = $repository;
-        $this->_plugin_version = $plugin_version;
-    }
+	public function __construct( Mondula_Form_Wizard_Wizard_Repository $repository, $plugin_version ) {
+		$this->_repository = $repository;
+		$this->_plugin_version = $plugin_version;
+	}
 
-    public function get_by_id( $id ) {
-        $row = $this->_repository->find_by_id( $id );
-        return Mondula_Form_Wizard_Wizard::from_aa( json_decode( $row->json, true ), $this->_plugin_version, $row->version );
-    }
+	public function get_by_id( $id ) {
+		$row = $this->_repository->find_by_id( $id );
+		return Mondula_Form_Wizard_Wizard::from_aa( json_decode( $row->json, true ), $this->_plugin_version, $row->version );
+	}
 
-    public function get_all( ) {
-        return $this->_repository->find();
-    }
+	public function get_all() {
+		return $this->_repository->find();
+	}
 
-    public function get_as_json( $id ) {
-      $all = $this->get_all();
-        if ( ! empty( $id ) ) {
-            $wizard = $this->get_by_id( $id );
-        } else {
-            $title = '';
-            $wizard = new Mondula_Form_Wizard_Wizard();
-        }
-        return json_encode( array('wizard' => $wizard->as_aa() ) );
-    }
+	public function get_as_json( $id ) {
+		$all = $this->get_all();
+		if ( ! empty( $id ) ) {
+			$wizard = $this->get_by_id( $id );
+		} else {
+			$title = '';
+			$wizard = new Mondula_Form_Wizard_Wizard();
+		}
+		return json_encode(
+			array(
+				'wizard' => $wizard->as_aa(),
+			)
+		);
+	}
 
-    public function save( $id, $data ) {
-        $row = array();
-        $row['date'] = current_time( 'mysql' );
-        $row['json'] = json_encode( $data['wizard'] );
-        $row['version'] = $this->_plugin_version;
-        // var_dump ( $row );
-        if ( ! empty ( $id ) ) {
-            // echo "data" . PHP_EOL;
-            return $this->_repository->update( $id, $row );
-        } else {
-            return $this->_repository->save( $row );
-        }
-    }
+	public function save( $id, $data ) {
+		$row = array();
+		$row['date'] = current_time( 'mysql' );
+		$row['json'] = json_encode( $data['wizard'] );
+		$row['version'] = $this->_plugin_version;
+		// var_dump ( $row );
+		if ( ! empty( $id ) ) {
+			// echo "data" . PHP_EOL;
+			return $this->_repository->update( $id, $row );
+		} else {
+			return $this->_repository->save( $row );
+		}
+	}
 
-    public function delete( $id ) {
-      $this->_repository->delete( $id );
-    }
-    
-    public function duplicate( $id ) {
-      $this->_repository->duplicate( $id );
-    }
+	public function delete( $id ) {
+		$this->_repository->delete( $id );
+	}
+
+	public function duplicate( $id ) {
+		$this->_repository->duplicate( $id );
+	}
 
 }
