c4e76656b3c0c850782b992f3a49106bb9e39c5f
Lewe Ohlsen <mail@lewe.io>
2017-09-13 17:02:09 +0200

    validating registration

--- assets/js/frontend/frontend.js
+++ assets/js/frontend/frontend.js
@@ -191,7 +191,18 @@ jQuery(document).ready(function($) {
                         break;
                     case 'fw-checkbox':
                         checkboxSummary(summaryObj, $(element), title, required);
-                        break;
+						break;
+						
+
+
+
+
+
+
+
+
+
+
                     default:
                         break;
                 }
@@ -524,7 +535,7 @@ jQuery(document).ready(function($) {
             }
         }
         return valid;
-    }
+	}
 
     function validateStep(idx) {
         var valid = true;
@@ -562,7 +573,7 @@ jQuery(document).ready(function($) {
                         break;
                     case 'fw-submit':
                         valid = validateSubmit($element);
-                        break;
+						break;
                     default:
                         break;
                 }
@@ -583,19 +594,66 @@ jQuery(document).ready(function($) {
                     }
                 }
             }
-        );
+		);
 
         if (!stepValid) {
-            // TODO: custom message
             $('.fw-block-invalid').each(function(idx, element) {
                 if ($(element).find('.fw-block-invalid-alert').length < 1) {
                     $(element).append('<div class="fw-block-invalid-alert">' + err[1] + '</div>');
                 }
             });
             alertUser(err[0], false);
-        }
+		}
+		
+		// validate registration
+		var $registration = $('.fw-wizard-step[data-stepid="' + idx + '"] .fw-step-block[data-type=fw-registration]');
+		if ($registration) {
+			// validate registration if required or email/username field filled
+			if ($registration.attr('data-required') == 'true' 
+			|| $registration.find('[data-id=username]').val()
+			|| $registration.find('[data-id=email]').val()) {
+				if (validateRegistration($registration) === false) {
+					stepValid = false;
+				}
+			}
+			
+		}
         return stepValid;
-    }
+	}
+	
+	function validateRegistration($element) {
+		var email = $element.find('[data-id=email]').val();
+		var username = $element.find('[data-id=username]').val();
+		var valid = true;
+
+		var data = {
+			action: 'msfp_pre_validate_registration',
+			email: email,
+			username : username,
+			nonce: ajax.nonce
+		};
+
+		$.ajax({
+            type: 'POST',
+            url: ajax.ajaxurl,
+            data: data,
+            // contentType: false,
+            // processData: false,
+			dataType: "json",
+			async: false,
+            success: function(r) {
+				if (r.success == false) {
+					valid = false;
+					alertUser(r.error, false);
+				}
+            },
+            fail: function(resp) {
+				valid = false;				
+				warn('response', resp);
+				warn('responseText', resp.responseText);            }
+		});
+		return valid;
+	}
 
     function validate($wizard) {
         var formValid = true;
@@ -604,7 +662,7 @@ jQuery(document).ready(function($) {
             if (!validateStep(idx)) {
                 formValid = false;
             }
-        });
+		});
         return formValid;
     }
 
@@ -639,7 +697,12 @@ jQuery(document).ready(function($) {
             $('.fw-spinner').show();
             summary = getSummary($wizard);
             files = getAttachments();
-            email = $wizard.find('[data-id="email"]').first().val();
+			email = $wizard.find('[data-id="email"]').first().val();
+			
+
+
+
+
             sendEmail(summary, email, files);
         }
     }
