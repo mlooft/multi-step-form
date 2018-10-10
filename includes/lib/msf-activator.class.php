02dd0ddc51940ea9d24ac444653ff380fd8646b3
Marten Manthey <43784820+MManthey@users.noreply.github.com>
2018-10-10 14:30:43 +0200

    Comments and Formating

--- includes/lib/msf-activator.class.php
+++ includes/lib/msf-activator.class.php
@@ -11,29 +11,44 @@ class Mondula_Form_Wizard_Activator {
 
 	private static $name = "mondula_form_wizards";
 
+	/**
+	 * Sets up the Database for the Site.
+	 * @param boolean $network_wide Identifies, wether the Plugin is network-wide actived for Multisites.
+	 */
 	public static function activate( $network_wide ) {
 		self::setup_db( $network_wide );
 	}
 
+	/**
+	 * If its a Multisite and the Plugin is network-wide activated, create a Table for every Blog,
+	 * else create a Table for the Blog of the Site.  
+	 */
 	private static function setup_db( $network_wide ) {
 	  global $wpdb;
 
 	  if ( is_multisite() && $network_wide ) {
-		$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-		foreach ( $blog_ids as $blog_id ) {
-		  self::activate_for_blog( $blog_id );
-		}
+			$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+			foreach ( $blog_ids as $blog_id ) {
+				self::activate_for_blog( $blog_id );
+			}
 	  } else {
-		self::create_table();
+			self::create_table();
 	  }
 	}
 
+	/**
+	 * Switches to a Blog and creates a Table for it, then switches back.
+	 * @param integer $blog_id Identifies a Blog.
+	 */
 	public static function activate_for_blog( $blog_id ) {
 	  switch_to_blog( $blog_id );
 	  self::create_table();
 	  restore_current_blog();
 	}
 
+	/**
+	 * Builds a SQL-Request to create a table.
+	 */
 	private static function create_table( ) {
 	  global $wpdb, $charset_collate;
 
@@ -53,9 +68,12 @@ class Mondula_Form_Wizard_Activator {
 	  dbDelta( $sql );
 	}
 
+	/**
+	 * ???
+	 */
 	public static function drop_table( $tables, $blog_id ) {
 	  if ( empty( $blog_id ) || 1 == $blog_id || $blog_id != $GLOBALS['blog_id'] ) {
-		return $tables;
+			return $tables;
 	  }
 
 	  global $wpdb;
