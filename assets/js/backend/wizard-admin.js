d00281aae8e4ffe3480e6a7ef0d79f33d1b9d79c
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2018-10-11 16:42:13 +0200

    Bugfix: default maximum and minimum

--- assets/js/backend/wizard-admin.js
+++ assets/js/backend/wizard-admin.js
@@ -164,9 +164,9 @@
         numericHtml += '<input type="text" class="fw-text-label fw-block-label" placeholder="' + wizard.i18n.label + '" value="' + block.label + '"></input><br/>';
         numericHtml += '<label><input type="checkbox" class="fw-required"'+ isChecked(block.required) + '/>' + wizard.i18n.required + '</label>';
         numericHtml += '<label>' + wizard.i18n.numeric.minimum + '</label>';
-        numericHtml += '<input type="text" class="fw-numeric-minimum fw-block-label" placeholder="' + wizard.i18n.numeric.no_minimum + '" value="' + block.minimum + '" pattern="-?\\d*"></input><br/><br/>';
+        numericHtml += '<input type="text" class="fw-numeric-minimum fw-block-label" placeholder="' + wizard.i18n.numeric.no_minimum + '" value="' + (block.minimum ? block.minimum : '') + '" pattern="-?\\d*"></input><br/><br/>';
         numericHtml += '<label>' + wizard.i18n.numeric.maximum + '</label>';
-        numericHtml += '<input type="text" class="fw-numeric-maximum fw-block-label" placeholder="' + wizard.i18n.numeric.no_maximum + '" value="' + block.maximum + '" pattern="-?\\d*"></input><br/>';
+        numericHtml += '<input type="text" class="fw-numeric-maximum fw-block-label" placeholder="' + wizard.i18n.numeric.no_maximum + '" value="' + (block.maximum ? block.maximum : '') + '" pattern="-?\\d*"></input><br/>';
         return numericHtml;
       }
 
@@ -1090,7 +1090,9 @@
                 var block = $(renderBlock({
                     type: 'numeric',
                     label: '',
-                    value: ''
+					value: '',
+					minimum: '',
+					maximum: '',
                 }));
                 var $part = $(thickEvent.target).parents('.fw-step-part');
                 $part.find('.inside').append(block);
